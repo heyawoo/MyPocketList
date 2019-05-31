e0059f5fe6939ef70bf004773fc1a618c8d419cf
heyawoo <heyawoo@gmail.com>
2019-05-31 15:37:01 +0900

    init project

---
+++ src/main/java/com/mypocketlist/demo/MyPocketListApplication.java
@@ -0,0 +1,13 @@
+package com.mypocketlist.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPocketListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPocketListApplication.class, args);
+	}
+
+}
